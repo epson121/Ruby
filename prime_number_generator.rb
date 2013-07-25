8e2de014a5739ebf04d82270c28efd3bf9c60a59
epson121 <traxdater12@gmail.com>
2013-07-25 22:16:20 +0200

    added prime_number_generation

---
+++ prime_number_generator.rb
@@ -0,0 +1,28 @@
+
+#program that generates first n prime numbers
+
+def primes(n)
+	counter = 0
+	primes = []
+	start = 3
+	while counter < n do
+		prime = true
+		(2..start - 1).each do |v|
+			if start % v == 0
+				prime = false
+			end
+		end
+		if prime == true
+			counter += 1
+			primes << start
+			prime = false
+		end
+		start += 1
+	end
+	return primes
+end
+
+p primes(10)
+p primes(20)
+p primes(200)
+p primes(2000)
